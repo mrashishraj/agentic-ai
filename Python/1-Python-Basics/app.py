28f7f78ec0e3056ca9425f13785b7dc81f3bab9b
mrashishraj-we <mr.ashishrajyadav@gmail.com>
2026-06-12 11:20:15 +0530

    initial commit - clean

---
+++ Python/1-Python-Basics/app.py
@@ -0,0 +1,9 @@
+print(1+1)
+
+##this is a sigle line comment
+
+'''this the example of a multi line comment
+it works in the same way as the single line comment but it can be used for multiple lines in py file only 
+and it will not work in zupitar nootbook'''
+
+
